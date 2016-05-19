d736a6b2f2e95a2c38bc3d27ba809ee870d30093
Josh Cox <josh@webhosting.coop>
2016-05-19 09:14:56 -0500

    untar.shadded

---
+++ untar.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -f ../mkFreeIPA.tgz  ]; then
+  (>&2 echo "../mkFreeIPA.tgz not found! you will need to copy this from the Master after completing prepMaster on the Master")
+fi ;
+
+if [ -f ../mkFreeIPA.tgz  ]; then
+  cd ../; tar zxvf - mkFreeIPA.tgz
+  echo 0
+fi ;
